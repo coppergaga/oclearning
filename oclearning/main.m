a92cacda889c6755f1ad7f600efc7b6772147ffd
coppergaga <411998690@qq.com>
2019-07-04 11:21:11 +0800

    Initial Commit

---
+++ oclearning/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oclearning
+//
+//  Created by gaoxudong01 on 2019/7/4.
+//  Copyright © 2019 gaoxudong01. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
